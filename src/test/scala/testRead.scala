275db37b8deedc21e86a593fa2471ffac5418c1e
andrea_bianchi24 <andrea.bianchi24@studio.unibo.it>
2022-09-23 15:43:05 +0200

    added new import for Scala Chart usage. Also created a functionable way to chart a certain function starting from a specific element to another one, with a small animation

--- src/test/scala/testRead.scala
+++ src/test/scala/testRead.scala
@@ -1,5 +1,6 @@
 import org.junit.Test
 import org.junit.Assert.*
+import de.sciss.chart.api._
 
 class testRead {
   @Test
@@ -10,5 +11,24 @@ class testRead {
     //assertTrue(List("ciao", "ragazzi") == elements(0))
     //assertTrue(List("sono", "Andrea") == elements(1))
     //assertTrue(List(List))
+
+    /* metodo al momento brutto per convertire quanto trovare tutto in int e trovare il minimo*/
+    println(elements(1).min.map(el => el.toInt).min)
+
+
+    /*
+     * tramite questo codice mostro su grafico i valori della funziona f = sin(x) partendo dal valore più piccolo della seconda riga di test1
+     * fino al valore più grande della prima riga di test1 ( è ancora tutto molto da sistemare, ma almeno vediamo che siamo in grado di graficare con animazione)
+     */
+    val series = new XYSeries("f(x) = sin(x)")
+    val chart = XYLineChart(series)
+    chart.show()
+    for (xi <- elements(1).min.map(el => el.toInt).min to elements(1).max.map(el => el.toInt).max) {
+      swing.Swing onEDT {
+        val x = xi * 0.5
+        series.add(x, math.sin(x))
+      }
+      Thread.sleep(150)
+    }
   }
 }
